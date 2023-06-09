6938166343df729bc73ffa44aadbccbcdafb8de5
Aditya786Deep <113698363+Aditya786Deep@users.noreply.github.com>
2023-06-09 08:23:06 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,34 @@
+// The only way to learn a new programming language is by writing programs in it. --Dennis Ritchie,
+
+#include <stdio.h>  
+int hcf(int a,int b)
+{
+   int r,q;
+   r = a % b;
+   while(r!=0)
+   {
+      q = a / b;      
+      r = a % b;
+      if (r == 0)
+        break;  
+      a = b;
+      b = r; 
+   }
+   return b;
+}
+int main()
+{
+   int a,b,h;
+   printf("Enter value of a and b:");
+   scanf("%d %d",&a,&b);
+   h = hcf(a,b);
+   printf("HCF(%d,%d) = %d\n",a,b,h);
+   printf("LCM(%d,%d) = %d\n",a,b,a * b / h);
+
+   return 0;
+}
+   
+  
+   
+  
+
